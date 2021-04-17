d804596411f19920a4f6ef8699351e6bc82cbe1f
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-17 19:56:44 +0800

    Updating formatting

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-function resetGrid(){
-    let gridSquares = prompt("How many squares per a side do you want?");
+function resetGrid() {
+  let gridSquares = prompt("How many squares per a side do you want?");
 
-    if (gridSquares <= 100){
+  if (gridSquares <= 100) {
     // Find container to insert grid in to
     const gridContainer = document.querySelector("#container");
 
@@ -12,30 +12,31 @@ function resetGrid(){
 
     // Iterate through adding flex box with scaled width and height
     for (i; i < gridCount; i++) {
-    const gridElement = document.createElement("div");
-    gridElement.classList.add("grid-element");
-    gridElement.setAttribute("id", i);
-    gridElement.setAttribute("style", `height: ${size}vh; width: ${size}vw`);
-    gridContainer.appendChild(gridElement);
+      const gridElement = document.createElement("div");
+      gridElement.classList.add("grid-element");
+      gridElement.setAttribute("id", i);
+      gridElement.setAttribute("style", `height: ${size}vh; width: ${size}vw`);
+      gridElement.addEventListener(
+        "mouseover",
+        function (event) {
+          // highlight the mouseover target
+          event.target.setAttribute("class", "grid-element-hover");
+        },
+        false
+      );
+      gridContainer.appendChild(gridElement);
     }
-    }
-    else {
-        alert("The maximum number of squares is 100 per a side! Try again")
-    }
-    } 
+  } else {
+    alert("The maximum number of squares is 100 per a side! Try again");
+  }
+}
 
-
-
-// Set up a hover effect so that the grid divs change colour when your mouse passes over them
-container.addEventListener(
-  "mouseover",
-  function (event) {
-    // highlight the mouseover target
-    event.target.setAttribute("class", "grid-element-hover");
-    // // reset the color after a short delay
-    // setTimeout(function() {
-    //     event.target.setAttribute("class", "grid-element");
-    // }, 10);
-  },
-  false
-);
+// // Set up a hover effect so that the grid divs change colour when your mouse passes over them
+// container.addEventListener(
+//   "mouseover",
+//   function (event) {
+//     // highlight the mouseover target
+//     event.target.setAttribute("class", "grid-element-hover");
+//   },
+//   false
+// );
